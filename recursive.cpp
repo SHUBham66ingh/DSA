eb5da1631f4ef12f2e9d511ac0b15faa0579224c
Shubham singh <shubham1041singh@gmail.com>
2024-06-07 16:33:26 +0530

    ....

---
+++ recursive.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    for(int i=n;i>0;i--)
+    {
+        cout<<i<<"days left for birthday\n";
+    }
+
+    int ptr;
+    cout<<sizeof(ptr)<<endl;
+}

d94d570337e28848db297ba4cfc32b3558e86989
Shubham singh <shubham1041singh@gmail.com>
2024-06-07 23:19:20 +0530

    .....

--- recursive.cpp
+++ recursive.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
 using namespace std;
 
+
+
+void fun3(int n)
+{
+    
+       if(n==0)
+       return ;
+       cout<<n<<"DAYS LEFT FOR BIRTHDAY"<<endl;
+       fun3(n-1);
+}
 int main()
 {
-    int n=5;
-    for(int i=n;i>0;i--)
-    {
-        cout<<i<<"days left for birthday\n";
-    }
+//     int n=5;
+//     for(int i=n;i>0;i--)
+//     {
+//         cout<<i<<"days left for birthday\n";
+//     }
 
-    int ptr;
-    cout<<sizeof(ptr)<<endl;
+//     int ptr;
+//     cout<<sizeof(ptr)<<endl;
+
+        fun3(3);
 }

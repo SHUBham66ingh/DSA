8812f0890f41acb82c327058e018377551077725
Shubham singh <shubham1041singh@gmail.com>
2024-06-10 18:25:37 +0530

    ....

--- oops1.cpp
+++ oops1.cpp
@@ -3,32 +3,39 @@ using namespace std;
 
 class Student
 {
-   public:
+   private:
    string name;
    int age , roll_number;
    string grade;
+   public:
+   void  setname(string n)
+   {
+      name = n;
+   }
 };
 
 int main()
 {
+      // Student s1;
+      // s1.name  = "Rohit";
+      // s1.age = 14;
+      // s1.roll_number = 21;
+      // s1.grade = "A+";
+
+      // cout<<s1.age<<" "<<endl;;
+      // cout<<s1.name<<" ";
+
+      // Student s2;
+      // s2.name = "shubahm";
+      // s2.age = 14;
+      // s2.roll_number = 21;
+      // s2.grade = "A+";
+
+
+      // cout<<s2.age<<" "<<endl;;
+      // cout<<s2.name<<" ";
       Student s1;
-      s1.name  = "Rohit";
-      s1.age = 14;
-      s1.roll_number = 21;
-      s1.grade = "A+";
-
-      cout<<s1.age<<" "<<endl;;
-      cout<<s1.name<<" ";
-
-      Student s2;
-      s2.name = "shubahm";
-      s2.age = 14;
-      s2.roll_number = 21;
-      s2.grade = "A+";
-
-
-      cout<<s2.age<<" "<<endl;;
-      cout<<s2.name<<" ";
+      s1.setname("shubham");
 
       
 

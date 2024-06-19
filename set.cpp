7157548d6572fcea35d0a5862e10f0acde695ea5
Shubham singh <shubham1041singh@gmail.com>
2024-06-20 03:46:17 +0530

    .......

---
+++ set.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include <unordered_set>
+#include <iterator>  
+using namespace std;
+int main()
+{
+    int arr[]={7,8,4,6,0,1,2};
+    unordered_set<int>set(arr.begin() , arr.end());
+
+}
